d7440c22674e283f6cf7ad2f98cfee0eceb44555
yk <nyarurato202@gmail.com>
2020-07-10 11:25:01 +0900

    study methods

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ var app = new Vue({
   data: {
     messages: {
       count: 100,
+      t: 'HELLO VUEEEE',
     },
     currentTab: 2
+  },
+  methods: {
+    reverseMessage: function () {
+      this.messages.t = this.messages.t.split('').reverse().join('');
+    },
+    changeTab: function() {
+      this.currentTab = (this.currentTab == 3 ) ? 1 : this.currentTab + 1; 
+    }
   }
 })
